0ffe422e646154af2242b4fa70359e4a9fcb0321
David Grammatico <david.lugdunum@gmail.com>
2024-02-03 18:45:52 +0100

    first commit

---
+++ lib/supabase/server.ts
@@ -0,0 +1,17 @@
+import { Database } from '@/types/supabase';
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+
+const cookieStore = cookies();
+
+export const supabase = createServerClient<Database>(
+	process.env.NEXT_PUBLIC_SUPABASE_URL!,
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+	{
+		cookies: {
+			get(name: string) {
+				return cookieStore.get(name)?.value;
+			},
+		},
+	}
+);
