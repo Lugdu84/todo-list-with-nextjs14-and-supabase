55d07b576bce1064b6de9ba96fde82bf35a7128b
David Grammatico <david.lugdunum@gmail.com>
2024-02-20 16:20:45 +0100

    add checkTodo

--- lib/todos/actions.ts
+++ lib/todos/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { createActionServer } from '@/lib/supabase/actions';
+import { Todo } from '@/types/todo';
 import { revalidatePath } from 'next/cache';
 
 export const addTodo = async (name: string) => {
@@ -28,6 +29,20 @@ export const addTodo = async (name: string) => {
 export const deleteTodo = async (id: number) => {
 	const supabase = createActionServer();
 	const { error } = await supabase.from('todos').delete().match({ id });
+	if (error) {
+		return {
+			message: error.message,
+		};
+	}
+	revalidatePath('/');
+};
+
+export const checkTodo = async (todo: Todo) => {
+	const supabase = createActionServer();
+	const { error } = await supabase
+		.from('todos')
+		.update({ is_completed: !todo.is_completed })
+		.match({ id: todo.id });
 	if (error) {
 		return {
 			message: error.message,
